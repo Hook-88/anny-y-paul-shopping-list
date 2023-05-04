6270b14fdddeb85b73d2cfe53d2d8ab00e2a5bf3
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2023-05-04 12:42:00 -0500

    Add logic to listen to change in database

--- script.js
+++ script.js
@@ -1,20 +1,52 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+//url of database
+const appSettings = {
+  databaseURL: "https://anny-y-paul-default-rtdb.firebaseio.com/"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+//second argument of ref is name of where we want to put the data
+const shoppingItemsInDB = ref(database, "shoppingList")
+
+
 const addBtn = document.getElementById('add-btn')
 const userInputField = document.getElementById('user-input-field')
 const shoppingListEl = document.getElementById('shopping-list-el')
 
-
 addBtn.addEventListener('click', renderShoppingList)
 
 function renderShoppingList(){
-  let item = capeltilizeFirstChar(userInputField.value) 
-  console.log(item)
+  const item = capeltilizeFirstChar(userInputField.value) 
   clearInputField()
-  renderShoppingItem(item)
-  
+  //fireBase method takes the reference and the item we want to add
+  push(shoppingItemsInDB, item)
+}
+
+function capeltilizeFirstChar(string) {
+  return string.charAt(0).toUpperCase() + string.substring(1).toLowerCase()
 }
 
 function clearInputField() {
   userInputField.value = ""
+}
+
+// when there's a change in de databse
+onValue(shoppingItemsInDB, function(snapshot) {
+  //snapshot.val returns an object.
+  //Object.values returns the values of object in an array
+  let shoppingListArray = Object.values(snapshot.val()) 
+  clearShoppingList()
+  
+  shoppingListArray.forEach(function(shoppingItem) {
+    renderShoppingItem(shoppingItem)
+  })
+})
+
+function clearShoppingList() {
+  shoppingListEl.innerHTML = ""
 }
 
 function renderShoppingItem(shoppingItem) {
@@ -22,8 +54,3 @@ function renderShoppingItem(shoppingItem) {
   listItemEl.textContent = shoppingItem
   shoppingListEl.append(listItemEl)
 }
-
-function capeltilizeFirstChar(string) {
-  return string.charAt(0).toUpperCase() + string.substring(1).toLowerCase()
-}
-
