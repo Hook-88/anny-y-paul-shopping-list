b4df00e746fb31f4874fa3358e5522053108c27f
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2023-05-04 11:39:20 -0500

    Improve render list-item logic

--- script.js
+++ script.js
@@ -6,9 +6,24 @@ const shoppingListEl = document.getElementById('shopping-list-el')
 addBtn.addEventListener('click', renderShoppingList)
 
 function renderShoppingList(){
-  let item = userInputField.value
-  const listItemEl = document.createElement('li')
-  listItemEl.textContent = item
-  shoppingListEl.append(listItemEl)
-
+  let item = capeltilizeFirstChar(userInputField.value) 
+  console.log(item)
+  clearInputField()
+  renderShoppingItem(item)
+  
 }
+
+function clearInputField() {
+  userInputField.value = ""
+}
+
+function renderShoppingItem(shoppingItem) {
+  const listItemEl = document.createElement('li')
+  listItemEl.textContent = shoppingItem
+  shoppingListEl.append(listItemEl)
+}
+
+function capeltilizeFirstChar(string) {
+  return string.charAt(0).toUpperCase() + string.substring(1).toLowerCase()
+}
+
